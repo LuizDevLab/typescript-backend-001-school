ff83d4ae09ff7b0beafc1026b1f99bc47dd69476
Luiz Henrique <luizgthub@gmail.com>
2025-09-09 20:03:07 -0300

    adding types

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,42 @@
 import { error } from "console";
 import express from "express";
+import { Express, Request, Response } from "express";
 
-const app = express();
+const app: Express = express();
+const port: Number = 3000;
+app.listen(port, () => {
+  console.log(`API está rodando na porta ${port}`);
+});
 
 app.use(express.json());
 
-const users = [
+type Usuario = {
+  id: number;
+  nome: string;
+  idade: number;
+};
+const users: Usuario[] = [
   { id: 1, nome: "ana", idade: 20 },
   { id: 2, nome: "Pedro", idade: 12 },
 ];
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Bem vindo a minha API!");
 });
 
-app.get("/users", (req, res) => {
+app.get("/users", (req: Request, res: Response) => {
   res.json(users);
 });
 
-app.put("/users/:id", (req, res) => {
+app.get("users/:id", (req: Request, res: Response) => {
+  
+})
+
+app.put("/users/:id", (req: Request, res: Response) => {
   const id = Number(req.params.id);
   const dadosAtualizados = req.body;
 
-  const userIndex = users.findIndex(user => user.id === id);
+  const userIndex = users.findIndex((user) => user.id === id);
 
   if (userIndex === -1) {
     return res.status(404).json({ error: "Usuário não encontrado" });
@@ -33,27 +47,22 @@ app.put("/users/:id", (req, res) => {
   res.json(users[userIndex]);
 });
 
-app.post("/users", (req, res) => {
-  const novoUsuario = req.body;
+app.post("/users", (req: Request, res: Response) => {
+  const novoUsuario: Usuario = req.body;
   users.push(novoUsuario);
   res.status(201).json(users);
 });
 
-app.delete("/users/:id", (req, res) => {
+app.delete("/users/:id", (req: Request, res: Response) => {
   const id = Number(req.params.id);
 
-  const userIndex =users.findIndex(user => user.id === id);
+  const userIndex = users.findIndex((user) => user.id === id);
 
   if (userIndex === -1) {
     return res.status(404).json({ error: "Usuário não encontrado" });
   }
 
-  const userRemovido = users.splice(userIndex, 1)
+  const userRemovido = users.splice(userIndex, 1);
 
-  res.json(userRemovido[0])
-})
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`API está rodando na porta ${port}`);
+  res.json(userRemovido[0]);
 });
